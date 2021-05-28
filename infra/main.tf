a53a7fcda481cdee17c6de6e270d99d1bf1845e5
Camilo Vera <chamix2@hotmail.com>
2021-05-28 01:32:23 -0300

    add storage account backend

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,14 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
+  backend "azurerm" {
+    resource_group_name   = "camilo-devops-training"
+    storage_account_name  = "camilosa202105250043"
+    container_name        = "terraform"
+    key                   = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      source = "hashicorp/azurerm"
+      version = "2.61.0"
     }
   }
 }
